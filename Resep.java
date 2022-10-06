3226c93029fc49b1a45079f3a5f0b59b355eb033
Agimmulya <Agimmulya>
2022-10-06 10:44:55 +0700

    inisiasi resep krabby patty

---
+++ Resep.java
@@ -0,0 +1,9 @@
+public class Resep {
+    public static void main(String[] args) {
+        System.out.println("Resep krabby patty");
+        System.out.println("------------------");
+        System.out.println("1. bawang");
+        System.out.println("2. daging");
+    }
+
+}
