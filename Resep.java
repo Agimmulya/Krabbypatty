8ed3efc49c014efad4ce2dd952c0c840eb736cda
Agimmulya <Agimmulya>
2022-10-06 11:05:11 +0700

    menambah resep

--- Resep.java
+++ Resep.java
@@ -4,6 +4,12 @@ public class Resep {
         System.out.println("------------------");
         System.out.println("1. bawang");
         System.out.println("2. daging");
+        System.out.println("2. bawang bombay");
+        System.out.println("2. keju mojarela");
+        System.out.println("2. salad");
+        System.out.println("2. acar");
     }
 
 }
+// berikutnya adalah modifikasi kode program tambahankan beberapa resep krabyatty
+// kemudian ketik git add, git commit -m sesuai pesan perubahan, git push origin main
